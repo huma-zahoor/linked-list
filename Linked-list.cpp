31bf9c073cf79884ba25367b110c314cf8eb843f
Huma Zahoor <humazahoor595@gmail.com>
2023-10-13 13:37:58 +0500

    Read and Write in the file

--- Linked-list.cpp
+++ Linked-list.cpp
@@ -1,4 +1,8 @@
 #include<iostream>
+#include <iomanip>
+#include <bits/stdc++.h> 
+#include <sstream>
+#include <fstream>
 using namespace std;
 class node
 {
@@ -6,7 +10,7 @@ class node
     string data;
     string data2;
     node* next;
-    node(string val,string val2)
+   node(string val,string val2)
     {
         data =val;
         data2 =val2;
@@ -15,8 +19,11 @@ class node
 };
 void InsertAtTail(node* &head, string val, string val2)
 {
+   
     node* nn = new node(val,val2);
+   
      nn->next = NULL;
+     
     if(head==NULL)
     {
         head =nn;
@@ -43,7 +50,6 @@ void InsertAtHead(node* &head,string val,string val2)
 }
 void InsertAtMiddle(node* &head,string val,string val2,int p)
 {
-  
     node* nn =new node(val,val2);
     if(head==NULL)
     {
@@ -59,7 +65,7 @@ void InsertAtMiddle(node* &head,string val,string val2,int p)
 nn->next = temp->next;
 temp->next = nn;
 }
-void DeleteFromHead(node* &head)
+/*void DeleteFromHead(node* &head)
 {
     head = head->next;
 }
@@ -71,77 +77,60 @@ void DeleteFromTail(node* &head)
         temp =temp->next;
     }
     temp->next =NULL;   
-}
-void DeleteFromMiddle(node* head,string pos)
+}*/
+void DeletionByName(node* &head,string pos)
 {
-    node* temp =head;
-    node* prev = NULL;
-   if (head->data==pos) 
+   
+  if(head==NULL)
+{
+    cout<< " List is empty!!!!"<<endl;
+
+}
+else if(head!=NULL)
+{
+    
+    if(head->data == pos)
     {
-        node* tem =head;
-        //head->data= head->next->data;
-      //  head->data2=head->data2;
-        head =tem->next;
-        delete tem;
+        node* temp = head; 
+        head = head->next;
+        delete temp;
+        return;
 
     }
     else
+
     {
-        while(temp->next !=NULL)
+         node* tem =head;
+         node* prev = NULL;
+        
+        while(tem!=NULL)
         {
-            if(temp->data==pos)
+           
+            if(tem->data==pos)
             {
-                prev->next =temp->next;
-                delete temp;
+                
+                prev->next =tem->next;
+                
+                delete tem;
                 return;
 
             }
             else
             {
-                prev =temp;
-                temp =temp->next;
+                prev =tem;
+                tem =tem->next;
                 
             }
 
         }
-        if(temp->data ==pos)
+        if(tem->data ==pos)
         {
             prev->next =NULL;
-            delete temp;
+            delete tem;
         }
-    }}
-/*for(int i=2; i< pos; i++) {
-  if(temp->next!=NULL) {
-    temp = temp->next;
-  }
-}*/
-//int i=1;
-//while (temp->next !=NULL)
-//{
-    /* code 
-    cout<<" hi"<<endl;
-    if (temp->data==pos)
-    { 
-        cout<<"hello"<< endl;
-        temp->next = temp->next->next;
-        cout << "kkk"<< endl;
-        //break;
     }
-    else
-   { 
-    cout<< "jjj"<<endl;
-    temp = temp->next;
-   }
-   // i++;
-}
 
-if (temp->data==pos)
-{
-
-temp->next = temp->next->next;
-}*/
-//}
-
+    }}
 void DisplayLinkedList(node* head) 
 {
      node* temp = head;
@@ -153,23 +142,99 @@ void DisplayLinkedList(node* head)
      }
      cout << "NULL"<<endl;
 }
+ bool SearchByName(node* &head,string val)
+    {
+      node* temp = head;
+      while(temp != NULL)
+      {
+        if (temp->data == val)
+        {
+            cout<<val<<" is found!!"<<endl;
+          return true;
+        }
+        temp = temp->next;
+      }
+      cout<<val<<" is not found!!"<<endl;
+      return false;
+    }
+    void WriteDataInTheFile(node* head)
+  {
+    ofstream file("List.txt");
+    node * temp = head;
+      //to print the key and data of every node in the list
+      while (temp != NULL)
+      {
+        file << temp -> data << setw(15) <<  temp -> data2;
+        temp = temp -> next;
+        file << endl;
+      }
+      cout << "----- Linked list backed up successfully -----" << endl << endl;
+
+  }
+  void ReadDataFreomFile(node* head)
+  {
+    ifstream file ;
+    file.open("List.txt",ios::in);
+    if(!file)
+    {
+        cout<<"File Dosen't exist!!!"<<endl;
+    }
+    else
+    {
+        
+        file.seekg(0, ios::beg);
+        string line;
+        string n1;
+        string d1;
+
+        while(file)
+        {
+            getline(file,line);
+          //  cout <<line<<endl;
+            stringstream obj_ss(line);
+     // getline() function to go through source string until it is finished.
+ 
+    getline(obj_ss, n1, ',');
+    
+    cout << n1 << endl;
+        int pos = line.find(",");
+        d1 = line.substr(pos+1);
+        cout << d1<< endl;
+        InsertAtTail(head,n1,d1);
+     //InsertAtHead(head,n1,d1);
+            if(file.eof())
+            {
+                break; 
+            }
+
+
+
+        }
+DisplayLinkedList(head);
+    }
+    file.close();
+
+  }
 int main()
 {
     node* head =NULL;
-    string num2,num3,pos1;
+    string num2,num3,pos1,pos2;
     int num ,pos;
     cout<<"******Linked List******"<<endl;
     while (1)
-    {  
+    {   
     cout << "******Menu******"<< endl;
     cout << "1. Insert at Head" << endl;
     cout << "2. Insert at Tail" << endl;
     cout << "3. Insert at any position" << endl;
-    cout << "4. Delete from Head" << endl;
-    cout << "5. Delete from Tails" << endl;
-    cout << "6. Deletion by name" << endl;
-    cout << "7. Show Linked List" << endl;
-    cout << "8. EXIT" <<endl; 
+    //cout << "4. Delete from Head" << endl;
+    //cout << "5. Delete from Tails" << endl;
+    cout << "4. Deletion by name" << endl;
+    cout << "5. Show Linked List" << endl;
+    cout << "6. Searching the list by name!!!" << endl;
+    cout << "7. Write data in the file"<<endl;
+    cout << "8. Read from file and write in the linked list"<<endl;
+    cout << "9. EXIT" <<endl; 
     cout << "Choose number from the above menu"<< endl;
     cin >> num;
     switch (num)
@@ -198,24 +263,38 @@ int main()
         cin >> pos;
         InsertAtMiddle(head,num2,num3,pos);
         break;
-    case 4:
+   /*case 4:
         
         DeleteFromHead(head);
         break;
     case 5:
         
         DeleteFromTail(head);
-        break;
-    case 6:
+        break;*/
+
+    case 4:
         cout << "Enter Name which you want to remove From the List"<<endl;
         getline(cin >> ws,pos1);//cin >> pos1;
-        DeleteFromMiddle(head,pos1);
-        cout << pos1 <<"is Deleted"<<endl;
+        DeletionByName(head,pos1);
+        cout << pos1 <<" is Deleted"<<endl;
         break;
-    case 7:
+    case 5:
          DisplayLinkedList(head);
          break;
-  case 8:
+    case 6:
+    cout << "Enter Name which you want to Search From the List"<<endl;
+        getline(cin >> ws,pos2);//cin >> pos1;
+        SearchByName(head,pos2);
+        break;
+         
+    case 7:
+        WriteDataInTheFile(head);
+        break;
+    case 8:
+    ReadDataFreomFile(head);
+    break;
+
+  case 9:
   exit(0);
       break;    
    }
